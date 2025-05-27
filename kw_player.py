aacecbda999d1a1765ea5253037847b9e389fb48
Konelgin <koko.kholeli@gmail.com>
2025-05-27 11:02:56 +1000

    ... or is it

--- kw_player.py
+++ kw_player.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json, datetime, sys
+import json, datetime, sys, random
 
 class Player:
     def __init__(self, topics):
@@ -29,20 +29,21 @@ class Player:
                         self.answering = self.player_data[self.name]['answering']
                         self.boss_questions = self.player_data[self.name]['boss_questions']
                         self.last_played = self.player_data[self.name]['last_played']
-                        target = datetime.datetime.strptime(self.last_played, "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=8)
-                        if datetime.datetime.now() > target:
-                            current = datetime.datetime.strptime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
-                            extra = round(((datetime.datetime.strptime(current-target, "%Y-%m-%d %H:%M:%S")).hour)%8)
-                            if extra > 1:
-                                extra *= 5
-                                if extra > 50:
-                                    extra = 50
-                            self.points += extra
-                        else:
-                            current = datetime.datetime.now()
-                            hours_left = (target - current).total_seconds() / 3600
-                            print(f'You have to wait another {round(hours_left)} hours.') # printing remaining hours till replay
-                            continue
+                        if self.last_played != 'interrupted':   
+                            target = datetime.datetime.strptime(self.last_played, "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=8)
+                            if datetime.datetime.now() > target:
+                                current = datetime.datetime.strptime(str(datetime.datetime.now()), "%Y-%m-%d %H:%M:%S")
+                                extra = round(((datetime.datetime.strptime(current-target, "%Y-%m-%d %H:%M:%S")).hour)%8)
+                                if extra > 1:
+                                    extra *= 5
+                                    if extra > 50:
+                                        extra = 50
+                                self.points += extra
+                            else:
+                                current = datetime.datetime.now()
+                                hours_left = (target - current).total_seconds() / 3600
+                                print(f'You have to wait another {round(hours_left)} hours.') # printing remaining hours till replay
+                                continue
                         BREAKING = True
                         break
 
@@ -65,26 +66,26 @@ class Player:
                         self.answering = {}
                         self.boss_questions = []
                         self.last_played = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
-                        # Save to file
-                        with open('kw_players.json', 'w') as f:
-                            json.dump(self.player_data, f)
                         self.report = []
                         BREAKING = True
                         break
                     # all players get assigned points and fresh new report    
             else:
                 while True:
-                    topic = input(f'Choose topic, q to quit: {topics}: ')
+                    topic = input(f'Choose topic, (q)uit: {topics}: ')
                     if topic in topics:
                         qa = json.load(open(f'kw_{topic}.json', 'r'))
+                        random.shuffle(qa)
                         for i in enumerate(qa, 1): 
-                            input(f'Q{i[0]}: {i[1][0]}: ')
+                            input(f'Q{i[0]}, (q)uit: {i[1][0]}: ')
                             print(f'The answer is: {i[1][1]}')
+                            
                     elif topic.lower() == 'q':
                         break
                     else:
                         print('Invalid topic.')
             if BREAKING: break
+            
             
                     
 
@@ -93,7 +94,7 @@ class Player:
         if self.gambling:
             while True:
                 try:
-                    self.gamble_amount = int(input(f'How much({self.points})? '))
+                    self.gamble_amount = int(input(f'Amount to bet (total: {self.points}): '))
                     if self.gamble_amount <= self.points:
                         self.points -= self.gamble_amount
                         break
@@ -105,13 +106,14 @@ class Player:
     def gamble_check(self):
         if self.gambling:
             if self.current_correct > 5:
-                print(f'You won {self.gamble_amount} points!')
+                print(f'\nYou gambled and you won {self.gamble_amount} pt!')
                 self.points += self.gamble_amount*2
             else:
-                print(f'You lost {self.gamble_amount} points.')
+                print(f'\nYou gambled and you lost {self.gamble_amount} pt.')
             self.gamble_amount = 0
 
-    def update_stat(self, time):
+
+    def update_stat(self, time, end):
         # this is called after a round of quiz is completed, to check level update
         if self.xp >= 60:
             self.xp -= 60
@@ -121,12 +123,19 @@ class Player:
             print('BOSS QA HOMING IN (maybe again)')
 
         # save all relevant info for the player/current session
-        self.player_data[self.name] = {'level':self.level, 'xp':self.xp, 'removed':self.removed, 'already_answered':self.already_answered, 'answering':self.answering, 'boss_questions':self.boss_questions, 'last_played':datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")}
+        last_played = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
+        if not end:
+            last_played = 'interrupted'
+        self.player_data[self.name] = {'level':self.level, 'xp':self.xp, 'removed':self.removed, 'already_answered':self.already_answered, 'answering':self.answering, 'boss_questions':self.boss_questions, 'last_played':last_played}
         json.dump(self.player_data, open('kw_players.json', 'w'))
         try:
-            contents = json.load(open(f'kw_{self.name}_report.json', 'r'))
+            try:
+                contents = json.load(open(f'kw_{self.name}_report.json', 'r'))
+            except:
+                contents = {}
+                json.dump({}, open(f'kw_{self.name}_report.json', 'w'))
             self.report.append({'Start': datetime.datetime.strftime(self.start, "%Y-%m-%d %H:%M:%S")})
-            contents[datetime.datetime.strftime(datetime.datetime.strptime(str(time)))] = self.report
+            contents[datetime.datetime.strftime(datetime.datetime.strptime())] = self.report
             json.dump(contents, open(f'kw_{self.name}_report.json', 'w'))
         except:
             json.dump({str(time):self.report}, open(f'kw_{self.name}_report.json', 'w'))
