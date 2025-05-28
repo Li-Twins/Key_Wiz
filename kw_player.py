ce6085e94a2c2a84e2803a9a013e078690fdd841
Ray <157849201+Ellisere@users.noreply.github.com>
2025-05-28 12:21:31 +0000

    all fixed, except 8hrs and Boss check

--- kw_player.py
+++ kw_player.py
@@ -2,17 +2,17 @@ from pathlib import Path
 import json, datetime, sys, random
 
 class Player:
-    def __init__(self, topics):
+    def __init__(self):
         while True:
             self.player_data = json.load(open('kw_players.json', 'r'))
             self.name = input('\nPlayer name: ')
             self.start = datetime.datetime.now()
-            self.current_correct = 0
             self.gamble_amount = 0
             self.gambling = False
             self.points = 20
             # load existing player 
             if self.name in list(self.player_data.keys()):
+                self.last_played = self.player_data[self.name]['last_played']
                 if self.last_played != 'interrupted':   
                     target = datetime.datetime.strptime(self.last_played, "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=8)
                     if datetime.datetime.now() > target:
@@ -35,7 +35,6 @@ class Player:
                 self.already_answered = self.player_data[self.name]['already_answered']
                 self.answering = self.player_data[self.name]['answering']
                 self.boss_questions = self.player_data[self.name]['boss_questions']
-                self.last_played = self.player_data[self.name]['last_played']
                 break
             # create new player with default values, save to list of players    
             else:
@@ -56,29 +55,10 @@ class Player:
                 self.answering = {}
                 self.boss_questions = []
                 self.last_played = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
-                self.report = []
                 break
+        self.report = []
 
-    def gamble(self):
-        while True:
-            self.gambling = input(f'\nHow much to gamble (out of {self.points})? ')
-            try:
-                self.gambling = int(self.gambling)
-                if self.gambling <= self.points:
-                    break
-                else:
-                    print('You don\'t have that much.')
-            except: 
-                print('Number please.')
 
-    def gamble_check(self):
-        if self.gambling:
-            if self.current_correct > 5:
-                print(f'\nYou gambled and you won {self.gamble_amount} pt!')
-                self.points += self.gamble_amount*2
-            else:
-                print(f'\nYou gambled and you lost {self.gamble_amount} pt.')
-            self.gamble_amount = 0
 
 
     def update_stat(self, time, end):
@@ -97,16 +77,15 @@ class Player:
         self.player_data[self.name] = {'level':self.level, 'xp':self.xp, 'removed':self.removed, 'already_answered':self.already_answered, 'answering':self.answering, 'boss_questions':self.boss_questions, 'last_played':last_played}
         json.dump(self.player_data, open('kw_players.json', 'w'))
         try:
-            try:
-                contents = json.load(open(f'kw_{self.name}_report.json', 'r'))
-            except:
-                contents = {}
-                json.dump({}, open(f'kw_{self.name}_report.json', 'w'))
-            self.report.append({'Start': datetime.datetime.strftime(self.start, "%Y-%m-%d %H:%M:%S")})
-            contents[datetime.datetime.strftime(datetime.datetime(1, 1, 1, 0, 0, 0, 0)+(datetime.datetime.now()-self.start), '%Y-%m-%d %H:%M:%S')] = self.report
-            json.dump(contents, open(f'kw_{self.name}_report.json', 'w'))
+            contents = json.load(open(f'kw_{self.name}_report.json', 'r'))
         except:
-            json.dump({str(time):self.report}, open(f'kw_{self.name}_report.json', 'w'))
+            contents = {}
+            json.dump({}, open(f'kw_{self.name}_report.json', 'w'))
+        # displays start time, duration, then record of the Quiz
+        start_time = datetime.datetime.strftime(self.start, "%Y-%m-%d %H:%M:%S")
+        duration = datetime.datetime.strftime(datetime.datetime(1, 1, 1, 0, 0, 0, 0)+(datetime.datetime.now()-self.start), '%H:%M:%S')
+        contents[f"Start at: {start_time}, Duration: {duration}"] = self.report
+        json.dump(contents, open(f'kw_{self.name}_report.json', 'w'))
         self.report = []
 
 
@@ -117,11 +96,11 @@ class Player:
     def remove_check(self, question, true):
         if question[0] in list(self.answering.keys()):
             if true == True:
-                if self.answering[question[0]] == 1:
+                if self.answering[question[0]] <= 1:
                     self.answering[question[0]] += 1
                 elif self.answering[question[0]] == 2:
                     self.removed.append(question[0])
-                    self.answering[question[0]] = 'removed'
+                    self.answering[question[0]] += 1
             else:
                 self.answering[question[0]] -= 1
         else:
--- kw_run.py
+++ kw_run.py
@@ -7,145 +7,176 @@ class Quiz:
     def __init__(self):
         self.start_time = datetime.datetime.now()
         self.topics = json.load(open('kw_topics.json', 'r'))
-        self.player = Player(self.topics)
-        
-        # Load topics with error handling
-        # Select topics based on player level
+        self.player = Player()
+        # Load topics with error handling, Select topics based on player level
         try:
             self.topics = self.topics[:self.player.level * 4]
         except IndexError:
             self.topics = self.topics  # Fallback if slicing fails
-
         self.questions = []
         self.topic = ''
         self.done_topics = []
+        self.rolled = []
+        self.current_correct = 0
 
 
     def topic_selection(self):
-        global game_on
-        # prompt if no topic exists
-        if not self.topics:
-            print('\nNo topic exists yet.')
-            game_on = False
-        # check if enough points to run quiz, with option to change to new topic
-        elif self.player.points >= 15:
-            self.player.points -= 15
-            # first gamble action occurs here
-            self.player.gamble()
-            while True:
-                try:
-                    # ensures the same topic will be repeated in next reroll
-                    self.topic = random.choice([x for x in self.topics if x != self.topic and not x in self.done_topics])
-                except:
-                    # exit run when no topic exists that has not been marked as complete (ie at least 10 questions not completed)
-                    print('\nNo unfinished topic at this moment.')
-                    sys.exit()
-
-                # generate available questions from comparing with quesions removed from given topic
-                available_questions = []
-                for i in json.load(open(f'kw_{self.topic}.json', 'r')):
-                    if not i[0] in self.player.removed:
-                        available_questions.append(i)
-                if len(available_questions) >= 10:
-                    print(f'\nYour topic is {self.topic}.')
-                    if self.player.points >= 5:
-                        if not input(f'\nRandomize topic for 5/{self.player.points} points, Y/N?: ').lower() == 'y':
-                            self.questions = available_questions
-                            break
-                        self.player.points -= 5 
-                    else:
-                        self.questions = available_questions
+        while True:
+            try:
+                # ensures the same topic won't be repeated in next reroll
+                self.topic = random.choice([x for x in self.topics if x != self.topic and not (x in self.done_topics) and not (x in self.rolled)])
+            except:
+                # exit run when no topic exists that has not been marked as complete (ie at least 10 questions not completed)
+                print('\nNo unfinished topic at this moment.')
+                sys.exit()
+            # generate available questions from comparing with quesions removed from given topic
+            available_questions = []
+            for i in json.load(open(f'kw_{self.topic}.json', 'r')):
+                if not i[0] in self.player.removed:
+                    available_questions.append(i)
+            if len(available_questions) >= 10:
+                print(f'\nYour topic is {self.topic}.')
+                if self.player.points >= 5:
+                    if not input(f'\nRandomize topic for 5/{self.player.points} points, Y/N?: ').lower() == 'y':
                         break
+                    self.player.points -= 5 
+                    self.rolled.append(self.topic)
                 else:
-                    # topics with less than 10 incompleted quesions are removed for the run, this behaviour needs to be reworked
-                    print(f'You have finished {self.topic}.')
-                    for i in available_questions:
-                        if not i[0] in list(self.player.answering.keys()):
-                            self.player.boss_questions.append(i)
-                    self.done_topics.append(self.topic)
-        # ends game when topics exist yet not enough points        
-        else:
-            print(f"Sorry, but you don't have enough points to continue on.")
-            game_on = False
-    
-    def boss(self):
-        if self.player.level == 10:
-            self.boss_qa(self.player.boss_questions)
-        else:
-            self.answer_quiz(self.questions)
+                    break
+            else:
+                # topics with less than 10 incompleted quesions are removed for the run, this behaviour needs to be reworked
+                print(f'You have finished {self.topic}.')
+                for i in available_questions:
+                    if not i[0] in list(self.player.answering.keys()):
+                        self.player.boss_questions.append(i)
+                self.done_topics.append(self.topic)
 
-    def boss_qa(self, qa):
-        if qa:
-            print(f'\n#### Boss QA has started ####')
-            while input('Continue boss QA? Y/N: ').lower() == 'y' and len(self.player.boss_questions) >= 10:
-                self.current_boss_qa = random.sample(qa, k=10)
-                while self.points >= 5:
-                    if input('Randomize questions? Y/N? ').lower() == 'y':
-                        self.points -= 5
+
+    def level_boss_check(self):
+        # needs extensive testing, not sure how or if it works, 
+        if self.player.level >= 10:
+            qa = self.player.boss_questions
+            if qa:
+                print(f'\n#### Boss QA has started ####')
+                while True:
+                    x = input('Ready for ten questions? (Y)es or (Q)uit: ')
+                    if x.lower() == "q":
+                        self.player.update_stat(datetime.datetime(1, 1, 1, 0, 0, 0, 0)+(datetime.datetime.now()-self.start_time), True)
+                        sys.exit()
+                    if x == 'y' and len(self.player.boss_questions) >= 10:
                         self.current_boss_qa = random.sample(qa, k=10)
-                    else:
-                        break
-                for question in enumerate(self.current_boss_qa, 1):
-                    player_answer = input(f'\nQ{question[0]}: {question[1][1]}: ') 
-                    if input(f"Answer:{question[1][1]}, (C)orrect?: ").lower() == 'c':
-                        self.player.points += 2
-                    self.player.report.append([question[1][0], player_answer])
-
-
+                        while self.points >= 5:
+                            if input('Randomize questions? Y/N? ').lower() == 'y':
+                                self.points -= 5
+                                self.current_boss_qa = random.sample(qa, k=10)
+                            else:
+                                break
+                        for question in enumerate(self.current_boss_qa, 1):
+                            player_answer = input(f'\nQ{question[0]}: {question[1][1]}: ') 
+                            if input(f"Answer:{question[1][1]}, (C)orrect?: ").lower() == 'c':
+                                self.player.points += 2
+                            self.player.report.append([question[1][0], player_answer])
+            
 
     def question_selection(self):
-        # retrieve 10 random questions from topic file, and display them
+        # load the questions from selected topic, get 10 samples
+        self.questions = json.load(open(f'kw_{self.topic}.json', 'r'))
         self.questions = random.sample(self.questions, k=10) # does sample exceed size traceback still occur?
-        for question in enumerate(self.questions, 1):
-            print(f'Q{question[0]}: {question[1][0]}')
+        for i in enumerate(self.questions, 1):
+            print(f'Q{i[0]}: {i[1][0]} ')
         # offer chance to randomize the list of questions, if so repeat the whole process
         if self.player.points >= 5:
             if input(f'\nRandomize questions for 5/{self.player.points} points, Y/N?: ').lower() == 'y':
                 self.player.points -= 5
-                self.questions = []
                 self.question_selection() 
-
 
 
     def answer_quiz(self, qa):
         # display each question and save player response 
-        answer_list = []
         self.start_time = datetime.datetime.now()
         print(f'\n#### Quiz has started at {self.start_time} ####')
         for i in range(10): 
             player_answer = input(f'\nQ{i+1}: {qa[i][0]}: ') 
-            answer_list.append(player_answer) # inputting
-            self_mark = input(f"Answer:{qa[i][1]}, (C)orrect?: ")
-
-
+            self_mark = input(f"Answer: {qa[i][1]} - (C)orrect?: ")
         # show each question, answer and player input to be self marked, calculate correct xp and points earned, save all the report
-        for i in range(10):
-            self.player.report.append([qa[i][0], answer_list[i]])
+            self.player.report.append([qa[i][0], player_answer])
             # for each correctly xp question
             if self_mark.lower() == "c":
                 self.player.points += 2
                 # update gamble counter
-                self.player.current_correct += 1
+                self.current_correct += 1
                 # already_answered > already_answered, xp > xp
                 if not qa[i][0] in self.player.already_answered:
                     self.player.xp += 1
                 self.player.remove_check(qa[i], True)
-                self.player.answering[qa[i][0]] = 'removed'
                 self.player.already_answered.append(qa[i][0])
             else:
                 # if incorrect, deduct one for the question
-                self.player.remove_check(qa[i], False)
-            
+                self.player.remove_check(qa[i], False)  
         # evaluate gamble result at the end of a run
-        self.player.gamble_check()
-        print(f"\nPoints remaining: {self.player.points}")
-        self.player.current_correct = 0
+        self.gamble_check()
+        print(f"Points remaining: {self.player.points}\n")
+        self.current_correct = 0
+
+
+    def pt_check(self):
+        # checks and deduct if enough points to play a quiz
+        if self.player.points >= 15:
+            self.player.points -= 15
+            return True
+        else:
+            print ("Not enough points for another quiz, bye\n")
+            return False
+
+
+    def gamble(self):
+        while True:
+            gambling = input(f'\nHow much to gamble (out of {self.player.points})? ')
+            try:
+                gambling = int(gambling)
+                if gambling <= self.player.points:
+                    self.gamble_amount = gambling
+                    self.player.points -= gambling
+                    break
+                else:
+                    print('Not enough points.')
+            except: 
+                print('Number please.')
+
+
+    def gamble_check(self):
+        if self.current_correct > 5 and self.gamble_amount > 0:
+            print(f'\nYou gambled and you won {self.gamble_amount} pt!')
+            self.player.points += self.gamble_amount*2
+        elif self.current_correct <= 5 and self.gamble_amount > 0:
+            print(f'\nYou gambled and you lost {self.gamble_amount} pt.')
+        self.gamble_amount = 0
+
+
+    def run_quiz(self):
+        # the main flow
+        while True:
+            # run the boss fight if sufficient level, quits the program without progressing the flow
+            self.level_boss_check()
+            # run the quiz when enough points
+            if self.pt_check():
+                print("\nStarting Quiz..\n")
+                self.gamble()
+                self.topic_selection()
+                self.question_selection()
+                self.answer_quiz(self.questions)
+                # record the last played time at the end of a run
+                self.player.update_stat(datetime.datetime(1, 1, 1, 0, 0, 0, 0)+(datetime.datetime.now()-self.start_time), True)
+            # without enough points, return to root
+            else:
+                break
+
 
 def mode():
     # allows the options of Normal (enter player name), Dev (access with code) or Quit program
     while True:
         # this is the root level where end of normal run returns to
-        x = input("Choose mode - (N)ormal (D)ev (Q)uit : ")
+        x = input("\nChoose mode - (N)ormal (D)ev (Q)uit : ")
         # dev mode calls dev function if correct code entered
         if x.lower() == "d":
             if input("Enter code: ") == "781023":
@@ -158,14 +189,12 @@ def mode():
         # normal mode, the bulk of the script
         if x.lower() == "n":
             thequiz = Quiz()
-            thequiz.topic_selection() # modifies game_on if not enough points
-            if game_on == True:
-                thequiz.question_selection() 
-                thequiz.boss()
-                thequiz.player.update_stat(datetime.datetime(1, 1, 1, 0, 0, 0, 0)+(datetime.datetime.now()-thequiz.start_time), False)
+            if not thequiz.topics:
+                print('\nNo uncompleted topics.')
+                break
             else:
-                thequiz.player.update_stat(datetime.datetime(1, 1, 1, 0, 0, 0, 0)+(datetime.datetime.now()-thequiz.start_time), True)
-                game_on = True
+                thequiz.run_quiz()
+
 
 def dev():
     # choose topic, displays all questions within
@@ -189,5 +218,5 @@ def dev():
         
 if __name__ == "__main__":
     mode()
-            
+
 
